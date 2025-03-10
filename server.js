ab28be1fa6f6965e65a55465103b9ab58ea3c4ca
SurajNatekar28 <surajnatekar2812@gmail.com>
2025-03-11 00:34:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ app.get("/search", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
